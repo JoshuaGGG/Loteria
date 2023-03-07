293fe788e750707a01ca84e150970766d879d6b6
JoshuaGGG <116187555+JoshuaGGG@users.noreply.github.com>
2023-03-07 10:42:04 -0800

    Worked on some code
    all boards are updated

--- script.js
+++ script.js
@@ -10,17 +10,29 @@ const boards = document.getElementsByClassName("board");
 for (let i = 0; i < boards.length; i++) {
   boards[i].addEventListener("click", function () {
     const selectedBoard = this.getAttribute("data-board");
+    console.log("data-board");
     const boardSelection = document.getElementById("board-selection");
+    console.log("board-selection");
     const gameplay = document.getElementById("gameplay");
+    console.log("gameplay");
     const playerBoard = document.getElementById("player-board");
+    console.log("player-board");
     const cpu1Board = document.getElementById("cpu1-board");
+    console.log("cpu1-board");
     const cpu2Board = document.getElementById("cpu2-board");
+    console.log("cpu2-board");
     const score = document.getElementById("score");
+    console.log("score");
     const deck = document.getElementById("deck");
+    console.log("deck");
     const beanPlacementToken = document.getElementById("bean-placement-token");
+    console.log("bean-placement-token");
     boardSelection.classList.add("hidden", "fadeOut");
+    console.log("hidden", "fadeout");
     gameplay.classList.remove("hidden");
+    console.log("hidden");
     gameplay.classList.add("fadeIn");
+    console.log("fadeIn");
 
     const selectedBoardElement = document.getElementById(selectedBoard);
     if (selectedBoardElement) {
